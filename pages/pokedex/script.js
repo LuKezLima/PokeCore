561706c837123f4ccd7a4573d9f3c8f05922464c
LuKezLima <lucas.dbz.lima@Hotmail.com>
2022-06-26 14:22:12 -0300

    feat/api connection

---
+++ pages/pokedex/script.js
@@ -0,0 +1,50 @@
+const getPokemon = async (name) => {
+    let pokemonData = '';
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+        pokemonData = data;
+    })
+
+
+    const layout = `
+<div class="resultContainer" id='resultId'>
+<div class="pokeImage">
+    <img src="https://img.pokemondb.net/artwork/large/${pokemonData.name}.jpg" alt="imagem do ${name}">
+    <h3>${pokemonData.name}</h3>
+    <p>nr. ${pokemonData.id}</p>
+</div>
+
+<div class="pokeInfo">
+    <h3>Tipo: ${pokemonData.types[0].type.name}</h3>
+    <div class="weightTall">
+        <h3>Altura:  ${pokemonData.height / 10} M</h3>
+        <h3>Peso: ${pokemonData.weight / 10} KG</h3>
+    </div>
+    <hr>
+    <div class="habilidades">
+        <p><strong>Habilidades:</strong> ${pokemonData.moves.map(
+            (skill) => skill.move.name
+          )} </h2> </p>
+    </div>
+</div>
+</div>
+`
+
+const main = document.getElementById('main')
+main.insertAdjacentHTML( "beforeEnd",layout)
+}
+
+
+const btnSearch = document.getElementById('btnSearch')
+
+btnSearch.addEventListener('click', (e) => {
+    const input = document.getElementById('input')
+    e.preventDefault()
+    if(document.getElementById('resultId')){
+        document.getElementById('main').removeChild(document.getElementById('main').lastElementChild)
+    }
+    getPokemon(input.value)
+})
+
+
