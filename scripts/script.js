684ca80067aa24b500c963af71eab9c8be2c4cf9
LuKezLima <lucas.dbz.lima@Hotmail.com>
2022-06-28 22:32:48 -0300

    fix/sizes

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@
         const menu = document.getElementById('menu')
 
         menuHamburguer.addEventListener('click', () => {
-            alert('chegou')
+           
             if(menu.style.display == 'grid'){
                 menu.style.display = 'none'
                 menuHamburguer.src = './assets/menuHamburguer.png'
             } else{
                 menu.style.display = 'grid'
                 menuHamburguer.src = './assets/closeIcon.png'
-                menuHamburguer.style.width = '50px'
+                menuHamburguer.style.width = '35px'
             }
 
 
