7aeabc17ed602be2c83a0c7f73d449951444e1c0
LuKezLima <lucas.dbz.lima@Hotmail.com>
2022-06-28 22:02:32 -0300

    fix/ham

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,10 @@
         menuHamburguer.addEventListener('click', () => {
             if(menu.style.display == 'grid'){
                 menu.style.display = 'none'
-                menuHamburguer.style.backgroundImage = 'url(/assets/menuHamburguer.png)'
+                menuHamburguer.style.backgroundImage = 'url(./assets/menuHamburguer.png)'
             } else{
                 menu.style.display = 'grid'
-                menuHamburguer.style.backgroundImage = 'url(/assets/closeIcon.png)'
+                menuHamburguer.style.backgroundImage = 'url(./assets/closeIcon.png)'
             }
 
 
