e810af4b625c86f8f245a45b6a769d13ac4c9d0d
PedroBoscachi <pedroboscachi@gmail.com>
2022-06-29 23:01:49 -0300

    arrumado dominio para pokecore

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,6 @@
             var emailInput = document.getElementById("form-email").value;
             var aparicao = emailInput.substring(emailInput.indexOf("@") + 1, emailInput.length)
             if(aparicao != "pokemons.com.br") {
-                alert("O domínio precisa ser pokemons.com.br");
+                alert("O domínio precisa ser pokecore.com.br");
             }
         }
